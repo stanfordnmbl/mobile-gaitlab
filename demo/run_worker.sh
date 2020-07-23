fa9170686cfbf3d5441c44c5960f9440ab28df85
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2020-07-23 15:48:49 +0000

    Solo worker

--- demo/run_worker.sh
+++ demo/run_worker.sh
@@ -1,2 +1,2 @@
 #docker build . -t stanfordnmbl/video-cp
-docker run -d --link gaitlab_redis_1:redis --link gaitlab_www_1:www --net gaitlab_default --gpus all -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $(pwd)/in:/gaitlab/input -v $(pwd)/out:/gaitlab/output stanfordnmbl/video-cp /bin/sh -c 'celery -A worker worker --loglevel=info --pool=solo'
+docker run --link gaitlab_redis_1:redis --link gaitlab_www_1:www --net gaitlab_default --gpus all -d --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $(pwd)/in:/gaitlab/input -v $(pwd)/out:/gaitlab/output stanfordnmbl/video-cp /bin/sh -c 'celery -A worker worker --loglevel=info --concurrency=1 --pool=solo'
