718bf254e3ed4e96795efefc601b25ebe14c8b1f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-24 11:01:11 +0800

    Delete demo directory

--- demo/run_local.sh
+++
@@ -1,5 +0,0 @@
-# GPU version
-docker run --gpus all -it --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -v $(pwd)/in:/gaitlab/input -v $(pwd)/out:/gaitlab/output stanfordnmbl/video-cp /bin/sh -c 'python3 demo.py'
-
-# CPU version -- not available, requires a docker with the OpenPose CPU version 
-# docker run -v $(pwd)/in:/gaitlab/input -v $(pwd)/out:/gaitlab/output stanfordnmbl/video-cp /bin/sh -c 'python3 demo.py'
